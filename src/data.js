6fdf7f228799011b80df2e38d64c1a4e14d32635
Kumar Ankur <Kumar@ld.ge>
2021-06-23 16:11:20 +0200

    First commit

---
+++ src/data.js
@@ -0,0 +1,45 @@
+export const projects = [
+  {
+    title: "SQL-Case Study",
+    subtitle: "Danny's Dinner",
+    description:
+      "Danny's Dinner is the project in which there are multiple tables in STAR Schema. Queried the database using different joins and complex queries",
+    image: "./project-1.png",
+    link: "https://github.com/ankurkum93/CaseStudy-SQL",
+  },
+  {
+    title: "Advanced Data Management",
+    subtitle: "Cassandra",
+    description:
+      "This is a mobile metro app that has been developed from scratch including data generation, cleaning, ingestion in Cassandara and then querying it",
+    image: "./project-2.jfif",
+    link: "https://github.com/ankurkum93/ADM",
+  },
+  {
+    title: "Data Warehousing",
+    subtitle: "Spark, Hive, Postgre SQL and Tableau",
+    description:
+      "This app features data cleaning, ingestion in distributed system and querying it with Spark, Hive and Postgre. Tableau is used for Data Viz",
+    image: "./project-3.jpg",
+    link: "https://github.com/ankurkum93/Data-Warehousing",
+  },
+  {
+    title: "Maven Toys Challenge",
+    subtitle: "Excel, SQL and Power BI",
+    description:
+      "This was an online dataset of Toy sales in Mexico from the year 1994-2018. I cleaned the data in Excel and transformation is done partly in SQL and Partly in Power BI",
+    image: "./project-4.jpg",
+    link: "https://github.com/ankurkum93/MavenChallenge",
+  },
+];
+
+
+
+export const skills = [
+  "Apache NiFi",
+  "SQL",
+  "Power BI",
+  "Tableau",
+  "Neo4J",
+  "Cassandara",
+];
