44787ad4801497ff754ccbc90bbd1918d0cd4a3a
Kumar Ankur <Kumar@ld.ge>
2021-06-27 23:15:07 +0200

    Eighth Commit

--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
 export const projects = [
   {
-    title: "SQL-Case Study",
-    subtitle: "Danny's Dinner",
+    title: "SQL-Portfolio Project",
+    subtitle: "Danny's Diner",
     description:
-      "Danny's Dinner is the project in which there are multiple tables in STAR Schema. Queried the database using different joins and complex queries",
+      "Danny's Diner is the project in which there are multiple tables in STAR Schema. Queried the database using different joins and complex queries",
     image: "./project-1.png",
     link: "https://github.com/ankurkum93/CaseStudy-SQL",
   },
   {
-    title: "Advanced Data Management",
+    title: "Mobile Metro app",
     subtitle: "Cassandra",
     description:
       "This is a mobile metro app that has been developed from scratch including data generation, cleaning, ingestion in Cassandara and then querying it",
@@ -16,7 +16,7 @@ export const projects = [
     link: "https://github.com/ankurkum93/ADM",
   },
   {
-    title: "Data Warehousing",
+    title: "Bike MS Project",
     subtitle: "Spark, Hive, Postgre SQL and Tableau",
     description:
       "This app features data cleaning, ingestion in distributed system and querying it with Spark, Hive and Postgre. Tableau is used for Data Viz",
